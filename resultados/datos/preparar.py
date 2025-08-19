821daf638eff9798d32f7683a0ec2871e2d59e4a
Mauricio Foronda <josemauricioforonda@gmail.com>
2025-08-19 18:22:49 -0400

    test

--- resultados/datos/preparar.py
+++ resultados/datos/preparar.py
@@ -40,19 +40,16 @@ def preparar_resultados():
     with open(os.path.join(base, "resultados.json"), "w") as f:
         json.dump(data, f, ensure_ascii=False)
 
+    total_actas = 35253
+    completo = admin.shape[0] / total_actas
+    with open(os.path.join(base, "progreso"), "w") as f:
+        f.write(str(completo))
+
 
 def preparar_recintos():
     recintos = gpd.read_file("./geo/2025/recintos.gpkg")
     recintos["c"] = identificar(recintos, "asiento", "recinto")
     recintos[["c", "geometry"]].to_file(os.path.join(base, "recintos.geojson"))
-
-
-def preparar_progreso():
-    padron_total = 7937138
-    df = pd.read_csv(os.path.join(base, ELECCION, "participacion.csv"))
-    completo = df.VotoEmitido.sum() / padron_total
-    with open(os.path.join(base, "progreso"), "w") as f:
-        f.write(str(completo))
 
 
 base = os.path.dirname(__file__)
