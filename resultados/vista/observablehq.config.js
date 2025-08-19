76c805cb363ca6c756928ae31f7c0e517215f03b
Mauricio Foronda <josemauricioforonda@gmail.com>
2025-08-19 18:54:42 -0400

    publicar

--- resultados/vista/observablehq.config.js
+++ resultados/vista/observablehq.config.js
@@ -1,8 +1,8 @@
-// See https://observablehq.com/framework/config for documentation.
 export default {
-  title: "resultados",
+  title: "Bolivia 2025",
   head: '<link rel="icon" href="https://mauforonda.github.io/images/icon.svg" type="image/svg" sizes="32x32">',
   root: "src",
   footer: "",
-  output: "../../docs"
+  output: "../../docs",
+  base: "/elecciones2025"
 };
